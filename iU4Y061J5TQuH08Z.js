ea07d06c2ba3acb76c583eb0a845a35e7c80ecf9
mob41 <puihuenlaw@gmail.com>
2019-01-26 23:31:30 +0800

    test

--- iU4Y061J5TQuH08Z.js
+++ iU4Y061J5TQuH08Z.js
@@ -1,13 +1,20 @@
 if (window.location.hash) {
 	var s = document.createElement("script");
-	s.src = "//unpkg.com/xhook@latest/dist/xhook.min.js";
+	s.src = "https://unpkg.com/ajax-hook/dist/ajaxhook.min.js";
 	s.onload = function () {
-		xhook.before(function (request) {
-			var x = window.location.hash.substring(1);
-			var k = "raw.githubusercontent.com/" + x;
-			console.log(request.url);
-			console.log(window.location.hostname);
-			request.url.replace(window.location.hostname, k);
+		hookAjax({
+			onreadystatechange: function (xhr) {
+				console.log("onreadystatechange called: %O", xhr)
+			},
+			onload: function (xhr) {
+				console.log("onload called: %O", xhr)
+			},
+			open: function (arg, xhr) {
+				console.log("open called: method:%s,url:%s,async:%s", arg[0], arg[1], arg[2])
+				var x = window.location.hash.substring(1);
+				var k = "raw.githubusercontent.com/" + x;
+				arg[1].replace(window.location.hostname, k);
+			}
 		});
 
 		var p = window.location.hash.substring(1);
