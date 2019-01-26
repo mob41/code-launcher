d74ca973841ac75516cc7b5277ad79fa4781ef9c
mob41 <puihuenlaw@gmail.com>
2019-01-26 23:26:05 +0800

    fix

--- iU4Y061J5TQuH08Z.js
+++ iU4Y061J5TQuH08Z.js
@@ -2,15 +2,17 @@ if (window.location.hash) {
 	var s = document.createElement("script");
 	s.src = "//unpkg.com/xhook@latest/dist/xhook.min.js";
 	s.onload = function () {
+		xhook.before(function (request) {
+			var x = window.location.hash.substring(1);
+			var k = "raw.githubusercontent.com/" + x;
+			console.log(request.url);
+			console.log(window.location.hostname);
+			request.url.replace(window.location.hostname, k);
+		});
+
 		var p = window.location.hash.substring(1);
 		var req = new XMLHttpRequest();
 		req.addEventListener("load", function () {
-			var k = "raw.githubusercontent.com/" + p;
-			xhook.before(function (request) {
-				console.log(request.url);
-				console.log(window.location.hostname);
-				request.url.replace(window.location.hostname, k);
-			});
 			document.getElementsByTagName("html")[0].innerHTML = this.responseText;
 		});
 		req.open("GET", "https://raw.githubusercontent.com/" + p + "/index.html");
