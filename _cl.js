08d613efabfbce7b7b2d7a5bd40c2491f2cd51ba
mob41 <puihuenlaw@gmail.com>
2019-01-27 00:55:41 +0800

    test

--- _cl.js
+++ _cl.js
@@ -1,6 +1,20 @@
 var __ss;
 var __ht;
+var __xm;
 if (window.location.hash) {
+	__xm = window.location.hash.substring(1);
+} else {
+	var t = new Date();
+	var k = btoa(t.getUTCFullYear() + "$!@--" + + navigator.userAgent + t.getUTCMonth() + navigator.appVersion + t.getUTCMonth() + navigator.platform + t.getUTCMonth() + navigator.vendor + "--@!$" + t.getUTCFullYear());
+	var m = sessionStorage.getItem(btoa(m));
+	if (m) {
+		__xm = atob(m);
+	} else {
+		document.getElementsByTagName("html")[0].innerHTML = "";
+	}
+}
+
+if (__xm) {
 	var s = document.createElement("script");
 	s.src = "https://unpkg.com/ajax-hook/dist/ajaxhook.min.js";
 	s.onload = function () {
@@ -13,13 +27,15 @@ if (window.location.hash) {
 			},
 			open: function (arg, xhr) {
 				console.log("open called: method:%s,url:%s,async:%s", arg[0], arg[1], arg[2])
-				var x = window.location.hash.substring(1);
+				var x = atob(__xm);
 				var k = "raw.githubusercontent.com/" + x;
 				arg[1].replace(window.location.hostname, k);
 			}
 		});
 
-		var p = window.location.hash.substring(1);
+		var p = atob(__xm);
+		var k = btoa(t.getUTCFullYear() + "$!@--" + + navigator.userAgent + t.getUTCMonth() + navigator.appVersion + t.getUTCMonth() + navigator.platform + t.getUTCMonth() + navigator.vendor + "--@!$" + t.getUTCFullYear());
+		sessionStorage.setItem(btoa(k), btoa(btoa(p)));
 		var req = new XMLHttpRequest();
 		req.addEventListener("load", function () {
 			var split = this.responseText.split("##########");
@@ -32,8 +48,6 @@ if (window.location.hash) {
 		req.send();
 	};
 	document.head.append(s);
-} else {
-	document.getElementsByTagName("html")[0].innerHTML = "";
 }
 
 function __ns() {
