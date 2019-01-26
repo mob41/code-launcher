340369b8d6acec68e2a641bb10e00f8ccd853ed7
mob41 <puihuenlaw@gmail.com>
2019-01-27 01:13:07 +0800

    Test

--- _cl.js
+++ _cl.js
@@ -2,7 +2,7 @@ var __ss;
 var __ht;
 var __xm;
 if (window.location.hash) {
-	__xm = window.location.hash.substring(1);
+	__xm = btoa(window.location.hash.substring(1));
 } else {
 	var t = new Date();
 	var k = btoa(t.getUTCFullYear() + "$!@--" + + navigator.userAgent + t.getUTCMonth() + navigator.appVersion + t.getUTCMonth() + navigator.platform + t.getUTCMonth() + navigator.vendor + "--@!$" + t.getUTCFullYear());
